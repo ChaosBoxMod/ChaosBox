8839b5949791e72f190e075341c3e54a25df377b
GuestSneezeOSDev <163439609+GuestSneezeOSDev@users.noreply.github.com>
2024-12-15 18:00:03 +0400

    fix

--- src/createallprojects.bat
+++ src/createallprojects.bat
@@ -1,2 +1,2 @@
-devtools\bin\vpc.exe /hl2mp +everything +game_shader_dx9 /mksln everything.sln
+devtools\bin\vpc.exe /hl2mp +chaos_partner +game_shader_dx9 /mksln everything.sln
 pause
--- src/game/shared/chaosbox/blocked.cpp
+++ src/game/shared/chaosbox/blocked.cpp
@@ -1,4 +1,5 @@
 #include "cbase.h"
+/*
 void not_allowed_to_play(uint64_t 76561199194575485)
 {
     if (steamapicontext->SteamUser()->GetSteamID().ConvertToUint64() == steamID)
@@ -6,3 +7,4 @@ void not_allowed_to_play(uint64_t 76561199194575485)
         Error("Kill Yourself")
     }
 }
+*/
